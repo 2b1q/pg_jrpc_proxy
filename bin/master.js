5d92a0e3ddbd9829bbabded308abe5f1218f692f
2b1q <i.shevtsov@bankex.com>
2018-11-29 14:10:49 +0300

    add JSON-RPC client request

--- bin/master.js
+++ bin/master.js
@@ -1,9 +1,21 @@
+/*
+ * Master process behavior
+ * 0. [MASTER] init RPC channel connection
+ * 1. [MASTER] handel RPC channel events
+ * 2. [MASTER] pass events to random [WORKER] process
+ * 3. [WORKER] do something
+ * 4. [MASTER] handle MSG from worker
+ * 5. [MASTER] exec RPC callback done(err,data)
+ * */
 let cluster = require("cluster"),
   cfg = require('../config/config'),
   { store, color: c } = cfg,
   { redis: redis_cfg, channel } = store;
 
-// if worker 'disconnect' from IPC channel
+/*
+* [WORKER] respawner
+* if worker 'disconnect' from IPC channel
+* */
 cluster.on("disconnect", (worker, code, signal) => {
     console.log("Worker %d died. Respawn", worker.id);
     cluster.fork();
@@ -11,29 +23,36 @@ cluster.on("disconnect", (worker, code, signal) => {
 
 cluster.on("online", worker => console.log(c.magenta + "Worker %d " + c.white + "online", worker.id));
 // fork workers by CPU cores
-let cpuCount = require("os").cpus().length;
-for (let i = 0; i < cpuCount; ++i) cluster.fork();
+let workers = require("os").cpus().length;
+// Fork worker process
+for (let i = 0; i < workers; ++i) cluster.fork();
+// Send payload to Random worker
+const sendMsgToRandWorker = (payload) => cluster.workers[Math.floor(Math.random() * (workers - 1)) + 1].send(payload);
 
-/*
-* Master process behavior
-* - init RPC channel connection
-* - handel RPC channel events
-* - todo pass events/msgs to worker process
-* - todo worker do staff > exec RPC callback done(err,data)
-* */
-/** simple RPC behavior */
+/** REDIS RPC + cluster RPC chatting behavior */
 const node_rpc_channel = channel.jrpc('master');
 const redisRpc = require('node-redis-rpc');
-const msg = {
-  msg:'hello from JSON-RPC-proxy service '+ node_rpc_channel,
-};
 console.log(`[MASTER node]: Init RPC service "${node_rpc_channel}"`);
 const rpc = new redisRpc(redis_cfg);
 // RPC handler
 rpc.on(node_rpc_channel, ({ payload }, channel, done) =>{
-  if(payload) console.log(`[MASTER node] channel: "${channel}". RPC Data>>>\n`, payload);
-  // Trigger done handler to fire back rpc result
-  // - first arg:  error status
-  // - second arg: result data
-  done(null, msg );
+  if(payload) console.log(`${c.yellow}[MASTER node] channel: "${channel}". RPC Data>>>\n${c.white}`, payload);
+  else return done('no payload');
+  // send MSG to Random Worker
+  sendMsgToRandWorker(payload);
+  // MSG handler from WORKER
+  const messageHandler = ({ msg, worker }) => {
+    // check error from worker
+    if(msg.error) return done(error);
+    // Trigger done handler to fire back rpc result
+    // - first arg:  error status
+    // - second arg: result data
+    done(null, {
+      msg: msg,
+      worker: worker,
+      channel: node_rpc_channel
+    });
+  };
+  // handle message from worker
+  for (const id in cluster.workers) cluster.workers[id].once('message', messageHandler);
 });
--- bin/node_proxy_worker.js
+++ bin/node_proxy_worker.js
@@ -1,6 +1,18 @@
-// const
-//     node_proxy = require("../modules/node_interaction/node_rpc_client"),
-//     env = process.env.NODE_ENV;
+const
+    node_proxy = require("../modules/node_interaction/node_rpc_client"),
+    env = process.env.NODE_ENV;
+const cfg = require('../config/config'),
+    { color: c } = cfg;
+const worker = require("cluster").worker,
+    { id: wid } = worker; // access to cluster.worker.id
 
-const { id: wid } = require("cluster").worker; // access to cluster.worker.id
-
+// handle msg from master
+worker.on('message', (msg) => {
+  console.log(`${c.green}WORKER[${wid}] got MSG\n${c.white}`, msg);
+  let { method, params } = msg;
+  node_proxy.nodeRequest('btc', method, params)
+    .then(node_response => worker.send({
+      msg: { ...node_response },
+      worker: wid
+    })); // send node_response to master process
+  });
