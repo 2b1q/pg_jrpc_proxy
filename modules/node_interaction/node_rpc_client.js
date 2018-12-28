9049584dcb8e2ce1671e32e14c1d407bec492f97
2b3q <2bb3qq@gmail.com>
2018-12-28 17:10:49 +0300

    add ETH ethers client RPC wrapper

--- modules/node_interaction/node_rpc_client.js
+++ modules/node_interaction/node_rpc_client.js
@@ -7,6 +7,7 @@ const cfg = require("../../config/config"),
     wid = require("cluster").worker.id,
     nodeTimer = node => `${c.yellow}[timer]${c.magenta} ${node} node ${c.white}request time`,
     { Client } = require("bitcoin"),
+    { providers } = require("ethers"),
     { api_requests: log_api, error: log_err } = require("../../utils/logger")(module);
 
 // empty response pattern
@@ -34,33 +35,57 @@ const nodeRequester = (node_type, method, params, config) =>
     new Promise(resolve => {
         empty.error = null; // clear error
         let cmd = Object([{ method: method, params: params }]);
-        console.log("exec cmd: ", cmd);
-        let con = Object.create(null); // connection Object container
-        con = config
-            ? config // if we have config => construct connection Object from incoming RPC payload
-            : typeof nodes[node_type] === "object"
-            ? nodes[node_type] // define node type without config
-            : undefined;
-        console.log(`${c.green}WORKER[${wid}] send JSON-RPC request to ${c.yellow}${node_type}${c.green} node connection:\n${c.white}`, con);
-        // register timer
-        console.time(nodeTimer(node_type));
-        // construct connection
-        if (con) {
-            // construct node client connection Object
-            let client = new Client(con);
-            client.cmd(cmd, (err, data) => {
-                if (err) {
+        console.log(`${node_type} node. exec cmd: `, cmd);
+        // switch ETH or LTC/BTC type request
+        if (node_type === "eth") {
+            let { host, port, protocol, user, pass } = config;
+            let url = `${protocol}://${user}:${pass}@${host}:${port}`;
+            console.log(`${c.green}WORKER[${wid}] send JSON-RPC request to ${c.yellow}${node_type}${c.green} node connection URL:\n${c.white}`, url);
+            console.time(nodeTimer(node_type));
+            const ethProvider = new providers.JsonRpcProvider(url);
+            ethProvider
+                .send(method, params)
+                .then(data => {
+                    console.log(`${c.green}[${c.magenta}${node_type}${c.green}] node data: ${c.white}`, data);
+                    console.timeEnd(nodeTimer(node_type));
+                    return resolve({ result: data, error: null, id: null });
+                })
+                .catch(err => {
+                    console.timeEnd(nodeTimer(node_type));
                     console.error(`Error on ${node_type} client request:\n`, err);
                     log_err(logit(err));
                     // setup error
                     empty.error = err;
                     return resolve(empty);
-                }
-                console.log(`${c.green}[${c.magenta}${node_type}${c.green}] node data: ${c.white}`, data);
-                console.timeEnd(nodeTimer(node_type));
-                resolve({ result: data, error: null, id: null });
-            });
-        } else resolve(empty); // no config for this node_type
+                });
+        } else {
+            let con = Object.create(null); // connection Object container
+            con = config
+                ? config // if we have config => construct connection Object from incoming RPC payload
+                : typeof nodes[node_type] === "object"
+                ? nodes[node_type] // define node type without config
+                : undefined;
+            console.log(`${c.green}WORKER[${wid}] send JSON-RPC request to ${c.yellow}${node_type}${c.green} node connection:\n${c.white}`, con);
+            // register timer
+            console.time(nodeTimer(node_type));
+            // construct connection
+            if (con) {
+                // construct node client connection Object
+                let client = new Client(con);
+                client.cmd(cmd, (err, data) => {
+                    if (err) {
+                        console.error(`Error on ${node_type} client request:\n`, err);
+                        log_err(logit(err));
+                        // setup error
+                        empty.error = err;
+                        return resolve(empty);
+                    }
+                    console.log(`${c.green}[${c.magenta}${node_type}${c.green}] node data: ${c.white}`, data);
+                    console.timeEnd(nodeTimer(node_type));
+                    resolve({ result: data, error: null, id: null });
+                });
+            } else resolve(empty); // no config for this node_type
+        }
     });
 
 // export node CMD executor for ext modules
